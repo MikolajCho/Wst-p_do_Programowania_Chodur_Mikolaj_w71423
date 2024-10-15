c82109a0da22300e2d30cc559f8dcb41d591b452
MikolajCho <mik.chodur@protonmail.com>
2024-10-15 18:46:06 +0200

    Zadanie 2

--- Lab1.py
+++ Lab1.py
@@ -1,9 +1,9 @@
-'''
+
 #Zadanie 1 - Jakie uzyskujesz wyniki? Co oznaczają poszczególne operatory?
 
 #int (integer) - Reprezentuje liczby całkowite.
 #float (floating-point) - Reprezentuje liczby rzeczywiste.
-
+'''
 print ((1 + 2), type (1 + 2),"\n", # wynik: 3 int | dodawaie liczb całkowitych
        (1 + 4.5), type (1 + 4.5),"\n", # wynik: 5.5 float | dodawanie liczby całkowitej i rzeczywistej
        (3 / 2), type (3 / 2),"\n", # wynik: 1.5 float | dzielenie zwykłe
@@ -20,8 +20,27 @@ pierwiastków to liczby ułamkowe, np. 2**0.5 = 1.414. Nawet dokładne pierwiast
 
 #Zadanie 2 - Sprawdź i wyjaśnij działanie następujących instrukcji:
 '''
-print(int(3.0)
-      float(3)
-      float("3")
-      str(12.4)
-      bool(0))'''
+print (int(3.0),"\n", #konwertuje liczbę float na liczbę int
+      float(3),"\n", #konwertuje liczbę int na liczbę float
+      float("3"),"\n", #konwertuje string na liczbę float
+      str(12.4),"\n", #konwertuje liczbę float na string
+      bool(0) #konwertuje wartość na boolean, czyli false lub true
+      )
+'''
+# W pythonie bool zwraca false dla wartości zerowych, pustych obiektów lub none, a true dla wszystkich innych wartości.
+#Przykłady:
+'''
+print((bool(0)),"\n",# False
+(bool(0.0)),"\n",    # False
+(bool("")),"\n",     # False
+(bool([])),"\n",     # False
+(bool(None)),"\n",   # False
+(bool(1)),"\n",      # True
+(bool(-2)),"\n",     # True
+(bool(3.14)),"\n",   # True
+(bool("Hello")),"\n",# True
+(bool([1, 2]))       # True
+)
+'''
+
+#Zadanie 3
