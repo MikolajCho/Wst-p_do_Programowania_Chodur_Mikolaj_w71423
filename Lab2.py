51262c490b0bd5a6a1585ee0eaa56cda3db644c4
MikolajCho <mik.chodur@protonmail.com>
2024-10-29 11:53:00 +0100

    Add files via upload

--- Lab2.py
+++ Lab2.py
@@ -7,7 +7,7 @@ else:
     print("Użytkownik nie jest pełnoletni")
 '''
 #Zadanie 9 - Stwórz automatyczny cennik biletowy według warunków:
-
+'''
 wiek = int(input("Podaj swój wiek: "))
 
 if wiek <= 4:
@@ -20,3 +20,27 @@ else:
         print("Bilet kosztuje 15 zł.")
     else:
         print("Bilet kosztuje 20 zł.")
+'''
+
+'''Zadanie 10 - Napisz program porządkowania trzech liczb x, y, z podawanych przez użytkownika. Posortuj
+je od najmniejszej do największej, bez użycia wbudowanych funkcji.'''
+'''
+x = int(input("x: "))
+y = int(input("y: "))
+z = int(input("z: "))
+
+if x > y:
+    x, y = y, x
+if x > z:
+    x, z = z, x
+if y > z:
+    y, z = z, y
+print(x, y, z)
+'''
+
+'''Zadanie 11 - Napisz program rozwiązywania równania kwadratowego 𝑎𝑎𝑥𝑥2 + 𝑏𝑏𝑏𝑏 + 𝑐𝑐 = 0, gdzie a, b i c
+są współczynnikami podawanymi przez użytkownika.'''
+
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
