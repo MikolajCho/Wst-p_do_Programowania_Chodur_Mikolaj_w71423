54b78f2129aff9b4f180eb0869ca59f81b2c1377
MikolajCho <mik.chodur@protonmail.com>
2024-11-05 11:35:16 +0100

    Add files via upload

---
+++ Lab3.py
@@ -0,0 +1,51 @@
+'''Zadania z labolatorium 2 - 8, 9, 10, 11'''
+
+#Zadanie 8
+
+# def funkcja(x):
+#     return 2*x**2 - 5*x - 8
+
+# x_poczatkowe = -4
+# x_koncowe = 4
+# krok = 0.5
+
+# x = x_poczatkowe
+# while x <= x_koncowe:
+#     y = funkcja(x)
+#     print(f"x = {x:.1f}, y = {y:.2f}")
+#     x += krok
+
+#Zadanie 9
+
+# while True:
+#     liczba = int(input("Podaj liczbę całkowitą: "))
+#     if liczba < 0:
+#         print("Wychodzę z pętli")
+#         break
+
+#Zadanie 10
+
+# a = int(input("Podaj pierwszą liczbę: "))
+# b = int(input("Podaj drugą liczbę: "))
+
+# while a < b:
+#     print(a)
+#     a += 1
+# while a >= b:
+#     print(b)
+#     b += 1
+
+
+#Zadanie 11 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# a = int(input("Podaj pierwszą liczbę: "))
+# b = int(input("Podaj drugą liczbę: "))
+
+# while a < b:
+#     print(a)
+#     a += 1
+# while a >= b:
+#     print(b)
+#     b += 1
+    
+'''Zadania z labolatorium 3 - '''
